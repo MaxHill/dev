774cc1cbce52bb7239e16425a2309637394732a6
Max Hill <max@hills.nu>
2026-02-25 14:01:42 +0100

    feat: add qutebrowser config to configs

---
+++ configs/qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,11 @@
+# Qutebrowser configuration file
+# Documentation: https://qutebrowser.org/doc/help/settings.html
+
+# Load autoconfig (load settings from :set commands in qutebrowser)
+config.load_autoconfig()
+
+# Add your custom settings below
+# Example:
+# c.colors.webpage.darkmode.enabled = True
+# c.tabs.position = "top"
+# c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/?q={}"}
